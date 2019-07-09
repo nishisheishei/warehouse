37643628149f7e1a32d5e56fb029c6af6499c100
nishisheishei <16619947998@163.com>
2019-07-09 19:46:17 +0800

    REM适配-配置px转换REM

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <!-- <p>123</p> -->
+    <div class="box"></div>
     <router-view/>
   </div>
 </template>
@@ -12,5 +13,9 @@ export default {
 </script>
 
 <style scoped>
-
+.box {
+  width: 375px;
+  height: 200px;
+  background-color: yellow;
+}
 </style>
