b3a2e99634b217b7bf48de92bf1441fc92881205
nishisheishei <16619947998@163.com>
2019-07-16 15:14:01 +0800

    搜索-联想建议-基本完成

---
+++ src/api/search.js
@@ -0,0 +1,23 @@
+/**
+ * 搜索相关数据接口
+ */
+
+import request from '@/utils/request'
+
+//  获取搜索结果
+// export const aa = () => {
+//     return request({
+
+//     })
+// }
+
+// 获取联想建议（自动补全）
+export const getSuggestion = q => {
+  return request({
+    method: 'GET',
+    url: '/app/v1_0/suggestion',
+    params: {
+      q
+    }
+  })
+}
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -11,14 +11,16 @@
 
         <!-- 联想建议 -->
         <van-cell-group>
-            <van-cell title="hello" icon="location-o" />
-            <van-cell title="hello" icon="location-o" />
-            <van-cell title="hello" icon="location-o" />
+            <van-cell
+                v-for="item in suggestions"
+                :key="item"
+                :title="item"
+                icon="location-o" />
         </van-cell-group>
         <!-- /联想建议 -->
 
         <!-- 历史记录 -->
-        <van-cell-group>
+        <!-- <van-cell-group>
             <van-cell title="历史记录">
                 <van-icon
                     slot="right-icon"
@@ -26,17 +28,40 @@
                     style="line-height: inherit;"
                 />
             </van-cell>
-        </van-cell-group>
+        </van-cell-group> -->
         <!-- /历史记录 -->
     </div>
 </template>
 
 <script>
+import { getSuggestion } from '@/api/search'
+
 export default {
   name: 'SearchIndex',
   data () {
     return {
-      searchText: ''
+      // 请输入搜索关键词
+      searchText: '',
+      // 获取到的  联想建议补全
+      suggestions: []
+    }
+  },
+
+  //  监视用的
+  watch: {
+    //   有两个参数  newVue 和 oldVal  分别是  改变后的状态  改变前的状态
+    async searchText (newVue) {
+      newVue = newVue.trim() // 去除首尾空格
+
+      //   判断  当输入内容为空的时候 则什么都不管
+      if (!newVue) {
+        return
+      }
+
+      // 如果不为空的时候，则请求联想建议补全
+      const data = await getSuggestion(newVue)
+      // console.log(data)
+      this.suggestions = data.options
     }
   }
 }
